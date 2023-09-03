879989cef8cfc759785d590f1d2f3823bea882b6
bojanangelis <bojanangeleski98@gmail.com>
2023-09-03 20:14:44 +0200

    adding tailwind & refresh animation

--- izvolte-client/app/(tabs)/index.tsx
+++ izvolte-client/app/(tabs)/index.tsx
@@ -1,31 +1,53 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+import { Platform, RefreshControl, SafeAreaView, ScrollView } from 'react-native'
+import Lottie from 'lottie-react-native'
+import { useRef, useState } from 'react'
+import { Text } from '../../components/Themed'
 
 export default function TabOneScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
-  );
-}
+  const animationRef = useRef<Lottie>(null)
+  const [showAnimation, setShowAnimation] = useState<boolean>(true)
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
+  return (
+    <SafeAreaView className='flex-1 items-center justify-center'>
+      <ScrollView
+        style={{ flex: 1 }}
+        contentContainerStyle={{
+          paddingHorizontal: 10,
+          paddingTop: Platform.select({ android: 30, ios: 10 }),
+        }}
+        refreshControl={
+          <RefreshControl
+            onRefresh={() => {
+              setShowAnimation(true)
+              animationRef.current?.play()
+            }}
+            refreshing={false}
+            tintColor={'transparent'}
+          />
+        }
+      >
+        {showAnimation && (
+          <Lottie
+            ref={animationRef}
+            resizeMode='contain'
+            source={require('../../assets/animations/animation_lm0u1qb2.json')}
+            style={{ width: 120, height: 120 }}
+            loop={false}
+            autoPlay
+            onAnimationFinish={() => {
+              animationRef.current?.pause()
+              setTimeout(() => {
+                setShowAnimation(false)
+              }, 1000)
+            }}
+          />
+        )}
+        <Text>dadada</Text>
+        <Text>dadada</Text>
+        <Text>dadada</Text>
+        <Text>dadada</Text>
+        <Text>dadada</Text>
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
